7abd9d6215bc6945a8d559420b1c8e4dff0164dc
headfirst7 <jun.h1425@gmail.com>
2015-06-20 21:22:52 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+var unirest = require('unirest');
+var express = require('express');
+var events = require('events');
+
+var getFromApi = function(endpoint, args) {
+    var emitter = new events.EventEmitter();
+    unirest.get('https://api.spotify.com/v1/' + endpoint)
+           .qs(args)
+           .end(function(response) {
+               emitter.emit('end', response.body);
+            });
+    return emitter;
+};
+
+var getFromRelatedApi = function(id) {
+    var emitter = new events.EventEmitter();
+    unirest.get('https://api.spotify.com/v1/artists/' + id + "/related-artists")
+           .end(function(response) {
+               emitter.emit('end', response.body);
+            });
+    return emitter;
+};
+
+var app = express();
+app.use(express.static('public'));
+
+app.get('/search/:name', function(req, res) {
+    var searchReq = getFromApi('search', {
+        q: req.params.name,
+        limit: 1,
+        type: 'artist'
+    });
+
+    searchReq.on('end', function(item) {
+        var artist = item.artists.items[0];
+        var related = getFromRelatedApi(artist.id);
+        related.on('end', function(item) {
+          var artists = item.artists;
+          res.json(artists);
+        });
+    });
+
+    searchReq.on('error', function() {
+        res.sendStatus(404);
+    });
+});
+
+app.listen(8080);
